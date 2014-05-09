1446a692702fa0e656c2c89b793c74691e6198d0
Pierre Wieser <pwieser@trychlos.org>
2014-05-09 16:02:36 +0000

    Openbook: adding src/ui/my-utils.h
    git-svn-id: http://svn.trychlos.pwi/repo/openbook/trunk@3411 f27faf81-44ee-0310-b8db-9bb09b83fd87

---
+++ src/ui/my-utils.h
@@ -0,0 +1,45 @@
+/*
+ * Open Freelance Accounting
+ * A double-entry accounting application for freelances.
+ *
+ * Copyright (C) 2014 Pierre Wieser (see AUTHORS)
+ *
+ * Open Freelance Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Freelance Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Freelance Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ *
+ * $Id$
+ */
+
+#ifndef __MY_UTILS_H__
+#define __MY_UTILS_H__
+
+/**
+ * SECTION: my_utils
+ * @short_description: Miscellaneous utilities
+ * @include: ui/my-utils.h
+ */
+
+#include <gtk/gtk.h.h>
+
+G_BEGIN_DECLS
+
+GtkWidget *my_utils_container_get_child_by_name( GtkContainer *container, const gchar *name );
+GtkWidget *my_utils_container_get_child_by_type( GtkContainer *container, GType type );
+
+G_END_DECLS
+
+#endif /* __MY_UTILS_H__ */
