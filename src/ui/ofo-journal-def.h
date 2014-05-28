c8b13e9cc17b2175806577ff8c13c8c288ff6905
Pierre Wieser <pwieser@trychlos.org>
2014-05-28 10:49:29 +0000

    Openbook: adding src/ui/ofo-journal-def.h
    git-svn-id: http://svn.trychlos.pwi/repo/openbook/trunk@3525 f27faf81-44ee-0310-b8db-9bb09b83fd87

---
+++ src/ui/ofo-journal-def.h
@@ -0,0 +1,67 @@
+/*
+ * Open Freelance Accounting
+ * A double-entry accounting application for freelances.
+ *
+ * Copyright (C) 2014 Pierre Wieser (see AUTHORS)
+ *
+ * Open Freelance Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Freelance Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Freelance Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ *
+ * $Id$
+ */
+
+#ifndef __OFO_JOURNAL_DEF_H__
+#define __OFO_JOURNAL_DEF_H__
+
+/**
+ * SECTION: ofo_journal
+ * @short_description: #ofoJournal class definition.
+ * @include: ui/ofo-journal.h
+ *
+ * This class implements the Journal behavior, including the general
+ * DB definition.
+ */
+
+#include "ui/ofo-base-def.h"
+
+G_BEGIN_DECLS
+
+#define OFO_TYPE_JOURNAL                ( ofo_journal_get_type())
+#define OFO_JOURNAL( object )           ( G_TYPE_CHECK_INSTANCE_CAST( object, OFO_TYPE_JOURNAL, ofoJournal ))
+#define OFO_JOURNAL_CLASS( klass )      ( G_TYPE_CHECK_CLASS_CAST( klass, OFO_TYPE_JOURNAL, ofoJournalClass ))
+#define OFO_IS_JOURNAL( object )        ( G_TYPE_CHECK_INSTANCE_TYPE( object, OFO_TYPE_JOURNAL ))
+#define OFO_IS_JOURNAL_CLASS( klass )   ( G_TYPE_CHECK_CLASS_TYPE(( klass ), OFO_TYPE_JOURNAL ))
+#define OFO_JOURNAL_GET_CLASS( object ) ( G_TYPE_INSTANCE_GET_CLASS(( object ), OFO_TYPE_JOURNAL, ofoJournalClass ))
+
+typedef struct {
+	/*< private >*/
+	ofoBaseClass parent;
+}
+	ofoJournalClass;
+
+typedef struct _ofoJournalPrivate       ofoJournalPrivate;
+
+typedef struct {
+	/*< private >*/
+	ofoBase            parent;
+	ofoJournalPrivate *priv;
+}
+	ofoJournal;
+
+G_END_DECLS
+
+#endif /* __OFO_JOURNAL_DEF_H__ */
