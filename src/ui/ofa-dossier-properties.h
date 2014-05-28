0f112c5c3cd29c8f33ac82082de83e060eed5a81
Pierre Wieser <pwieser@trychlos.org>
2014-05-28 13:46:58 +0000

    Openbook: adding src/ui/ofa-dossier-properties.h
    git-svn-id: http://svn.trychlos.pwi/repo/openbook/trunk@3534 f27faf81-44ee-0310-b8db-9bb09b83fd87

---
+++ src/ui/ofa-dossier-properties.h
@@ -0,0 +1,71 @@
+/*
+ * Open Freelance Accounting
+ * A double-entry accounting application for freelances.
+ *
+ * Copyright (C) 2014 Pierre Wieser (see AUTHORS)
+ *
+ * Open Freelance Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Freelance Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Freelance Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ *
+ * $Id: ofa-dossier-properties.h 3521 2014-05-28 10:48:34Z  $
+ */
+
+#ifndef __OFA_DOSSIER_PROPERTIES_H__
+#define __OFA_DOSSIER_PROPERTIES_H__
+
+/**
+ * SECTION: ofa_dossier_properties
+ * @short_description: #ofaDossierProperties class definition.
+ * @include: ui/ofa-dossier-properties.h
+ *
+ * Update the dossier properties.
+ */
+
+#include "ui/ofa-main-window-def.h"
+#include "ui/ofo-dossier-def.h"
+
+G_BEGIN_DECLS
+
+#define OFA_TYPE_DOSSIER_PROPERTIES                ( ofa_dossier_properties_get_type())
+#define OFA_DOSSIER_PROPERTIES( object )           ( G_TYPE_CHECK_INSTANCE_CAST( object, OFA_TYPE_DOSSIER_PROPERTIES, ofaDossierProperties ))
+#define OFA_DOSSIER_PROPERTIES_CLASS( klass )      ( G_TYPE_CHECK_CLASS_CAST( klass, OFA_TYPE_DOSSIER_PROPERTIES, ofaDossierPropertiesClass ))
+#define OFA_IS_DOSSIER_PROPERTIES( object )        ( G_TYPE_CHECK_INSTANCE_TYPE( object, OFA_TYPE_DOSSIER_PROPERTIES ))
+#define OFA_IS_DOSSIER_PROPERTIES_CLASS( klass )   ( G_TYPE_CHECK_CLASS_TYPE(( klass ), OFA_TYPE_DOSSIER_PROPERTIES ))
+#define OFA_DOSSIER_PROPERTIES_GET_CLASS( object ) ( G_TYPE_INSTANCE_GET_CLASS(( object ), OFA_TYPE_DOSSIER_PROPERTIES, ofaDossierPropertiesClass ))
+
+typedef struct _ofaDossierPropertiesPrivate        ofaDossierPropertiesPrivate;
+
+typedef struct {
+	/*< private >*/
+	GObject                      parent;
+	ofaDossierPropertiesPrivate *private;
+}
+	ofaDossierProperties;
+
+typedef struct {
+	/*< private >*/
+	GObjectClass parent;
+}
+	ofaDossierPropertiesClass;
+
+GType    ofa_dossier_properties_get_type( void );
+
+gboolean ofa_dossier_properties_run     ( ofaMainWindow *parent, ofoDossier *dossier );
+
+G_END_DECLS
+
+#endif /* __OFA_DOSSIER_PROPERTIES_H__ */
