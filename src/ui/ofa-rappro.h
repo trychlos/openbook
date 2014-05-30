1c17b0ea37f2fc4f34e499585c59b708cf3a3f1e
Pierre Wieser <pwieser@trychlos.org>
2014-05-30 10:49:28 +0000

    Openbook: adding src/ui/ofa-rappro.h
    git-svn-id: http://svn.trychlos.pwi/repo/openbook/trunk@3553 f27faf81-44ee-0310-b8db-9bb09b83fd87

---
+++ src/ui/ofa-rappro.h
@@ -0,0 +1,68 @@
+/*
+ * Open Freelance Accounting
+ * A double-entry accounting application for freelances.
+ *
+ * Copyright (C) 2014 Pierre Wieser (see AUTHORS)
+ *
+ * Open Freelance Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Freelance Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Freelance Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ *
+ * $Id$
+ */
+
+#ifndef __OFA_RAPPRO_H__
+#define __OFA_RAPPRO_H__
+
+/**
+ * SECTION: ofa_rappro
+ * @short_description: #ofaRappro class definition.
+ * @include: ui/ofa-journals-set.h
+ *
+ * Display the chart of accounts, letting the user edit it.
+ */
+
+#include "ui/ofa-main-page-def.h"
+
+G_BEGIN_DECLS
+
+#define OFA_TYPE_RAPPRO                ( ofa_rappro_get_type())
+#define OFA_RAPPRO( object )           ( G_TYPE_CHECK_INSTANCE_CAST( object, OFA_TYPE_RAPPRO, ofaRappro ))
+#define OFA_RAPPRO_CLASS( klass )      ( G_TYPE_CHECK_CLASS_CAST( klass, OFA_TYPE_RAPPRO, ofaRapproClass ))
+#define OFA_IS_RAPPRO( object )        ( G_TYPE_CHECK_INSTANCE_TYPE( object, OFA_TYPE_RAPPRO ))
+#define OFA_IS_RAPPRO_CLASS( klass )   ( G_TYPE_CHECK_CLASS_TYPE(( klass ), OFA_TYPE_RAPPRO ))
+#define OFA_RAPPRO_GET_CLASS( object ) ( G_TYPE_INSTANCE_GET_CLASS(( object ), OFA_TYPE_RAPPRO, ofaRapproClass ))
+
+typedef struct _ofaRapproPrivate       ofaRapproPrivate;
+
+typedef struct {
+	/*< private >*/
+	ofaMainPage       parent;
+	ofaRapproPrivate *private;
+}
+	ofaRappro;
+
+typedef struct {
+	/*< private >*/
+	ofaMainPageClass parent;
+}
+	ofaRapproClass;
+
+GType ofa_rappro_get_type( void );
+
+G_END_DECLS
+
+#endif /* __OFA_RAPPRO_H__ */
