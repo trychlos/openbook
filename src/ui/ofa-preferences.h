12d13bd1bb484fa242b279306094596f182e903e
Pierre Wieser <pwieser@trychlos.org>
2014-07-13 06:37:43 +0000

    Openbook: adding src/ui/ofa-preferences.h
    git-svn-id: http://svn.trychlos.pwi/repo/openbook/trunk@3767 f27faf81-44ee-0310-b8db-9bb09b83fd87

---
+++ src/ui/ofa-preferences.h
@@ -0,0 +1,78 @@
+/*
+ * Open Freelance Accounting
+ * A double-entry accounting application for freelances.
+ *
+ * Copyright (C) 2014 Pierre Wieser (see AUTHORS)
+ *
+ * Open Freelance Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Freelance Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Freelance Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ *
+ * $Id$
+ */
+
+#ifndef __OFA_PREFERENCES_H__
+#define __OFA_PREFERENCES_H__
+
+/**
+ * SECTION: ofa_preferences
+ * @short_description: #ofaPreferences class definition.
+ * @include: ui/ofa-preferences.h
+ *
+ * Manage the general user preferences,
+ * i.e. user preferences which are not attached to any dossier.
+ */
+
+#include "ui/my-dialog.h"
+#include "ui/ofa-main-window-def.h"
+
+G_BEGIN_DECLS
+
+#define OFA_TYPE_PREFERENCES                ( ofa_preferences_get_type())
+#define OFA_PREFERENCES( object )           ( G_TYPE_CHECK_INSTANCE_CAST( object, OFA_TYPE_PREFERENCES, ofaPreferences ))
+#define OFA_PREFERENCES_CLASS( klass )      ( G_TYPE_CHECK_CLASS_CAST( klass, OFA_TYPE_PREFERENCES, ofaPreferencesClass ))
+#define OFA_IS_PREFERENCES( object )        ( G_TYPE_CHECK_INSTANCE_TYPE( object, OFA_TYPE_PREFERENCES ))
+#define OFA_IS_PREFERENCES_CLASS( klass )   ( G_TYPE_CHECK_CLASS_TYPE(( klass ), OFA_TYPE_PREFERENCES ))
+#define OFA_PREFERENCES_GET_CLASS( object ) ( G_TYPE_INSTANCE_GET_CLASS(( object ), OFA_TYPE_PREFERENCES, ofaPreferencesClass ))
+
+typedef struct _ofaPreferencesPrivate        ofaPreferencesPrivate;
+
+typedef struct {
+	/*< private >*/
+	myDialog               parent;
+	ofaPreferencesPrivate *private;
+}
+	ofaPreferences;
+
+typedef struct {
+	/*< private >*/
+	myDialogClass parent;
+}
+	ofaPreferencesClass;
+
+GType    ofa_preferences_get_type( void ) G_GNUC_CONST;
+
+gboolean ofa_preferences_run     ( ofaMainWindow *parent );
+
+/* these are helpers available to the rest of the application
+ */
+gboolean ofa_prefs_assistant_quit_on_escape   ( void );
+gboolean ofa_prefs_assistant_confirm_on_escape( void );
+gboolean ofa_prefs_assistant_confirm_on_cancel( void );
+
+G_END_DECLS
+
+#endif /* __OFA_PREFERENCES_H__ */
