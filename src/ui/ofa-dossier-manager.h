bec61d34bc56878672ed1664ed44390cc8d97bb5
Pierre Wieser <pwieser@trychlos.org>
2014-07-15 19:04:52 +0000

    Openbook: adding src/ui/ofa-dossier-manager.h
    git-svn-id: http://svn.trychlos.pwi/repo/openbook/trunk@3793 f27faf81-44ee-0310-b8db-9bb09b83fd87

---
+++ src/ui/ofa-dossier-manager.h
@@ -0,0 +1,71 @@
+/*
+ * Open Freelance Accounting
+ * A double-entry accounting application for freelances.
+ *
+ * Copyright (C) 2014 Pierre Wieser (see AUTHORS)
+ *
+ * Open Freelance Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Freelance Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Freelance Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ *
+ * $Id$
+ */
+
+#ifndef __OFA_DOSSIER_MANAGER_H__
+#define __OFA_DOSSIER_MANAGER_H__
+
+/**
+ * SECTION: ofa_dossier_manager
+ * @short_description: #ofaDossierManager class definition.
+ * @include: ui/ofa-dossier-manager.h
+ *
+ * Manage the existing dossiers.
+ */
+
+#include "ui/my-dialog.h"
+#include "ui/ofa-main-window-def.h"
+
+G_BEGIN_DECLS
+
+#define OFA_TYPE_DOSSIER_MANAGER                ( ofa_dossier_manager_get_type())
+#define OFA_DOSSIER_MANAGER( object )           ( G_TYPE_CHECK_INSTANCE_CAST( object, OFA_TYPE_DOSSIER_MANAGER, ofaDossierManager ))
+#define OFA_DOSSIER_MANAGER_CLASS( klass )      ( G_TYPE_CHECK_CLASS_CAST( klass, OFA_TYPE_DOSSIER_MANAGER, ofaDossierManagerClass ))
+#define OFA_IS_DOSSIER_MANAGER( object )        ( G_TYPE_CHECK_INSTANCE_TYPE( object, OFA_TYPE_DOSSIER_MANAGER ))
+#define OFA_IS_DOSSIER_MANAGER_CLASS( klass )   ( G_TYPE_CHECK_CLASS_TYPE(( klass ), OFA_TYPE_DOSSIER_MANAGER ))
+#define OFA_DOSSIER_MANAGER_GET_CLASS( object ) ( G_TYPE_INSTANCE_GET_CLASS(( object ), OFA_TYPE_DOSSIER_MANAGER, ofaDossierManagerClass ))
+
+typedef struct _ofaDossierManagerPrivate        ofaDossierManagerPrivate;
+
+typedef struct {
+	/*< private >*/
+	myDialog                  parent;
+	ofaDossierManagerPrivate *private;
+}
+	ofaDossierManager;
+
+typedef struct {
+	/*< private >*/
+	myDialogClass parent;
+}
+	ofaDossierManagerClass;
+
+GType ofa_dossier_manager_get_type( void ) G_GNUC_CONST;
+
+void  ofa_dossier_manager_run     ( ofaMainWindow *parent );
+
+G_END_DECLS
+
+#endif /* __OFA_DOSSIER_MANAGER_H__ */
