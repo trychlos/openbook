6e965fd78627564ae1cae25b15727c6e1cdf186b
Pierre Wieser <pwieser@trychlos.org>
2014-10-28 11:44:42 +0000

    Openbook: adding src/ui/ofa-page-prot.h
    git-svn-id: http://svn.trychlos.pwi/repo/openbook/trunk@3991 f27faf81-44ee-0310-b8db-9bb09b83fd87

---
+++ src/ui/ofa-page-prot.h
@@ -0,0 +1,55 @@
+/*
+ * Open Freelance Accounting
+ * A double-entry accounting application for freelances.
+ *
+ * Copyright (C) 2014 Pierre Wieser (see AUTHORS)
+ *
+ * Open Freelance Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Freelance Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Freelance Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ *
+ * $Id$
+ */
+
+#ifndef __OFA_PAGE_PROT_H__
+#define __OFA_PAGE_PROT_H__
+
+/**
+ * SECTION: ofa_page
+ * @short_description: #ofaPage class definition.
+ * @include: ui/ofa-page.h
+ */
+
+#include "api/ofo-dossier-def.h"
+
+#include "core/ofa-main-window-def.h"
+
+G_BEGIN_DECLS
+
+/* protected instance data
+ */
+struct _ofaPageProtected {
+	gboolean       dispose_has_run;
+
+	/* properties set at instanciation time
+	 */
+	ofaMainWindow *main_window;
+	ofoDossier    *dossier;
+};
+
+G_END_DECLS
+
+#endif /* __OFA_PAGE_PROT_H__ */
