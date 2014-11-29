91d2d8a7560c923528c1940ae7e96a719581d6fb
Pierre Wieser <pwieser@trychlos.org>
2014-11-29 14:13:27 +0000

    Openbook: adding src/api/ofa-dbms.h
    git-svn-id: http://svn.trychlos.pwi/repo/openbook/trunk@4181 f27faf81-44ee-0310-b8db-9bb09b83fd87

---
+++ src/api/ofa-dbms.h
@@ -0,0 +1,55 @@
+/*
+ * Open Freelance Accounting
+ * A double-entry accounting application for freelances.
+ *
+ * Copyright (C) 2014 Pierre Wieser (see AUTHORS)
+ *
+ * Open Freelance Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Freelance Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Freelance Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ *
+ * $Id$
+ */
+
+#ifndef __OFA_DBMS_H__
+#define __OFA_DBMS_H__
+
+/**
+ * SECTION: ofa_dbms
+ * @short_description: An object which handles the DBMS connexion
+ * @include: api/ofa-dbms.h
+ */
+
+#include "api/ofa-dbms-def.h"
+#include "api/ofa-idbms.h"
+
+G_BEGIN_DECLS
+
+ofaDbms *ofa_dbms_new               ( void );
+
+gboolean ofa_dbms_connect           ( ofaDbms *dbms,
+											const gchar *dname, const gchar *dbname,
+											const gchar *account, const gchar *password,
+											gboolean display_error );
+
+GSList  *ofa_dbms_get_exercices     ( ofaDbms *dbms,
+											const gchar *dname );
+
+#define  ofa_dbms_free_exercices(L) ofa_idbms_free_exercices( L )
+
+G_END_DECLS
+
+#endif /* __OFA_DBMS_H__ */
