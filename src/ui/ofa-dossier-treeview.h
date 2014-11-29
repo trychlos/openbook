360b633b667ffb7d0cfb49d4a78a7489dec0216a
Pierre Wieser <pwieser@trychlos.org>
2014-11-29 14:13:34 +0000

    Openbook: adding src/ui/ofa-dossier-treeview.h
    git-svn-id: http://svn.trychlos.pwi/repo/openbook/trunk@4188 f27faf81-44ee-0310-b8db-9bb09b83fd87

---
+++ src/ui/ofa-dossier-treeview.h
@@ -0,0 +1,79 @@
+/*
+ * Open Freelance Accounting
+ * A double-entry accounting application for freelances.
+ *
+ * Copyright (C) 2014 Pierre Wieser (see AUTHORS)
+ *
+ * Open Freelance Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Freelance Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Freelance Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ *
+ * $Id$
+ */
+
+#ifndef __OFA_DOSSIER_TREEVIEW_H__
+#define __OFA_DOSSIER_TREEVIEW_H__
+
+/**
+ * SECTION: ofa_dossier_treeview
+ * @short_description: #ofaDossierTreeview class definition.
+ * @include: ui/ofa-dossier-treeview.h
+ *
+ * Manage a treeview with the list of the dossiers which are defined in
+ * the settings.
+ */
+
+#include "core/ofa-main-window-def.h"
+
+G_BEGIN_DECLS
+
+#define OFA_TYPE_DOSSIER_TREEVIEW                ( ofa_dossier_treeview_get_type())
+#define OFA_DOSSIER_TREEVIEW( object )           ( G_TYPE_CHECK_INSTANCE_CAST( object, OFA_TYPE_DOSSIER_TREEVIEW, ofaDossierTreeview ))
+#define OFA_DOSSIER_TREEVIEW_CLASS( klass )      ( G_TYPE_CHECK_CLASS_CAST( klass, OFA_TYPE_DOSSIER_TREEVIEW, ofaDossierTreeviewClass ))
+#define OFA_IS_DOSSIER_TREEVIEW( object )        ( G_TYPE_CHECK_INSTANCE_TYPE( object, OFA_TYPE_DOSSIER_TREEVIEW ))
+#define OFA_IS_DOSSIER_TREEVIEW_CLASS( klass )   ( G_TYPE_CHECK_CLASS_TYPE(( klass ), OFA_TYPE_DOSSIER_TREEVIEW ))
+#define OFA_DOSSIER_TREEVIEW_GET_CLASS( object ) ( G_TYPE_INSTANCE_GET_CLASS(( object ), OFA_TYPE_DOSSIER_TREEVIEW, ofaDossierTreeviewClass ))
+
+typedef struct _ofaDossierTreeviewPrivate        ofaDossierTreeviewPrivate;
+
+typedef struct {
+	/*< public members >*/
+	GObject                    parent;
+
+	/*< private members >*/
+	ofaDossierTreeviewPrivate *priv;
+}
+	ofaDossierTreeview;
+
+typedef struct {
+	/*< public members >*/
+	GObjectClass               parent;
+}
+	ofaDossierTreeviewClass;
+
+GType               ofa_dossier_treeview_get_type ( void ) G_GNUC_CONST;
+
+ofaDossierTreeview *ofa_dossier_treeview_new      ( void );
+
+void                ofa_dossier_treeview_attach_to( ofaDossierTreeview *tview,
+															GtkContainer *new_parent );
+
+void                ofa_dossier_treeview_init_view( ofaDossierTreeview *tview,
+															const gchar *name );
+
+G_END_DECLS
+
+#endif /* __OFA_DOSSIER_TREEVIEW_H__ */
