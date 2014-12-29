2bc40e8a3ebb73f9f954bb94d878beb6699cd810
Pierre Wieser <pwieser@trychlos.org>
2014-12-29 18:45:46 +0000

    Openbook: adding src/mysql/ofa-mysql-connect-enter-piece.h
    git-svn-id: http://svn.trychlos.pwi/repo/openbook/trunk@4385 f27faf81-44ee-0310-b8db-9bb09b83fd87

---
+++ src/mysql/ofa-mysql-connect-enter-piece.h
@@ -0,0 +1,57 @@
+/*
+ * Open Freelance Accounting
+ * A double-entry accounting application for freelances.
+ *
+ * Copyright (C) 2014 Pierre Wieser (see AUTHORS)
+ *
+ * Open Freelance Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Freelance Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Freelance Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ *
+ * $Id$
+ */
+
+#ifndef __OFA_MYSQL_CONNECT_ENTER_PIECE_H__
+#define __OFA_MYSQL_CONNECT_ENTER_PIECE_H__
+
+/**
+ * SECTION: ofa_mysql_idbms
+ * @short_description: #ofaMysql class definition.
+ *
+ * #ofaIDbms interface management.
+ */
+
+#include <api/ofa-idbms.h>
+
+G_BEGIN_DECLS
+
+void     ofa_mysql_connect_enter_piece_attach_to   ( ofaIDbms *instance,
+																GtkContainer *parent,
+																GtkSizeGroup *group );
+
+gboolean ofa_mysql_connect_enter_piece_is_valid    ( const ofaIDbms *instance,
+																GtkContainer *parent );
+
+gchar   *ofa_mysql_connect_enter_piece_get_database( const ofaIDbms *instance,
+																GtkContainer *parent );
+
+gboolean ofa_mysql_connect_enter_piece_apply       ( const ofaIDbms *instance,
+																const gchar *dname,
+																void *infos );
+
+G_END_DECLS
+
+#endif /* __OFA_MYSQL_CONNECT_ENTER_PIECE_H__ */
