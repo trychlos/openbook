43f7061849a3350b27b73e385c0566f8d8c19716
Pierre Wieser <pwieser@trychlos.org>
2015-01-24 14:03:37 +0000

    Openbook: adding src/ui/ofa-check-integrity.h
    git-svn-id: http://svn.trychlos.pwi/repo/openbook/trunk@4513 f27faf81-44ee-0310-b8db-9bb09b83fd87

---
+++ src/ui/ofa-check-integrity.h
@@ -0,0 +1,73 @@
+/*
+ * Open Freelance Accounting
+ * A double-entry accounting application for freelances.
+ *
+ * Copyright (C) 2014 Pierre Wieser (see AUTHORS)
+ *
+ * Open Freelance Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Freelance Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Freelance Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ *
+ * $Id$
+ */
+
+#ifndef __OFA_CHECK_INTEGRITY_H__
+#define __OFA_CHECK_INTEGRITY_H__
+
+/**
+ * SECTION: ofa_check_integrity
+ * @short_description: #ofaCheckIntegrity class definition.
+ * @include: ui/ofa-check-integrity.h
+ *
+ * Check DBMS integrity.
+ */
+
+#include "core/my-dialog.h"
+#include "core/ofa-main-window-def.h"
+
+G_BEGIN_DECLS
+
+#define OFA_TYPE_CHECK_INTEGRITY                ( ofa_check_integrity_get_type())
+#define OFA_CHECK_INTEGRITY( object )           ( G_TYPE_CHECK_INSTANCE_CAST( object, OFA_TYPE_CHECK_INTEGRITY, ofaCheckIntegrity ))
+#define OFA_CHECK_INTEGRITY_CLASS( klass )      ( G_TYPE_CHECK_CLASS_CAST( klass, OFA_TYPE_CHECK_INTEGRITY, ofaCheckIntegrityClass ))
+#define OFA_IS_CHECK_INTEGRITY( object )        ( G_TYPE_CHECK_INSTANCE_TYPE( object, OFA_TYPE_CHECK_INTEGRITY ))
+#define OFA_IS_CHECK_INTEGRITY_CLASS( klass )   ( G_TYPE_CHECK_CLASS_TYPE(( klass ), OFA_TYPE_CHECK_INTEGRITY ))
+#define OFA_CHECK_INTEGRITY_GET_CLASS( object ) ( G_TYPE_INSTANCE_GET_CLASS(( object ), OFA_TYPE_CHECK_INTEGRITY, ofaCheckIntegrityClass ))
+
+typedef struct _ofaCheckIntegrityPrivate        ofaCheckIntegrityPrivate;
+
+typedef struct {
+	/*< public members >*/
+	myDialog                 parent;
+
+	/*< private members >*/
+	ofaCheckIntegrityPrivate *priv;
+}
+	ofaCheckIntegrity;
+
+typedef struct {
+	/*< public members >*/
+	myDialogClass            parent;
+}
+	ofaCheckIntegrityClass;
+
+GType ofa_check_integrity_get_type( void ) G_GNUC_CONST;
+
+void  ofa_check_integrity_run     ( ofaMainWindow *main_window );
+
+G_END_DECLS
+
+#endif /* __OFA_CHECK_INTEGRITY_H__ */
