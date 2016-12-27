df0431035e760d124b4805ccb5d9ec445ed92f68
Pierre Wieser <pwieser@trychlos.org>
2016-12-27 12:12:50 +0100

    ofaExerciceMetaBin: new class

---
+++ src/ui/ofa-exercice-meta-bin.c
@@ -0,0 +1,451 @@
+/*
+ * Open Firm Accounting
+ * A double-entry accounting application for professional services.
+ *
+ * Copyright (C) 2014,2015,2016 Pierre Wieser (see AUTHORS)
+ *
+ * Open Firm Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Firm Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Firm Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <glib/gi18n.h>
+
+#include "my/my-date.h"
+#include "my/my-date-editable.h"
+#include "my/my-utils.h"
+
+#include "api/ofa-hub.h"
+#include "api/ofa-idbprovider.h"
+#include "api/ofa-preferences.h"
+
+#include "ui/ofa-application.h"
+#include "ui/ofa-exercice-meta-bin.h"
+
+/* private instance data
+ */
+typedef struct {
+	gboolean         dispose_has_run;
+
+	/* initialization
+	 */
+	ofaHub          *hub;
+	gchar           *settings_prefix;
+	guint            rule;
+
+	/* UI
+	 */
+	GtkSizeGroup    *group0;
+
+	/* runtime data
+	 */
+	GDate            begin;
+	GDate            end;
+}
+	ofaExerciceMetaBinPrivate;
+
+/* signals defined here
+ */
+enum {
+	CHANGED = 0,
+	N_SIGNALS
+};
+
+static guint st_signals[ N_SIGNALS ]    = { 0 };
+
+static const gchar *st_resource_ui      = "/org/trychlos/openbook/ui/ofa-exercice-meta-bin.ui";
+
+static void setup_bin( ofaExerciceMetaBin *self );
+static void on_begin_changed( GtkEditable *editable, ofaExerciceMetaBin *self );
+static void on_end_changed( GtkEditable *editable, ofaExerciceMetaBin *self );
+static void on_current_toggled( GtkToggleButton *button, ofaExerciceMetaBin *self );
+static void changed_composite( ofaExerciceMetaBin *self );
+
+G_DEFINE_TYPE_EXTENDED( ofaExerciceMetaBin, ofa_exercice_meta_bin, GTK_TYPE_BIN, 0,
+		G_ADD_PRIVATE( ofaExerciceMetaBin ))
+
+static void
+exercice_meta_bin_finalize( GObject *instance )
+{
+	static const gchar *thisfn = "ofa_exercice_meta_bin_finalize";
+	ofaExerciceMetaBinPrivate *priv;
+
+	g_debug( "%s: instance=%p (%s)",
+			thisfn, ( void * ) instance, G_OBJECT_TYPE_NAME( instance ));
+
+	g_return_if_fail( instance && OFA_IS_EXERCICE_META_BIN( instance ));
+
+	/* free data members here */
+	priv = ofa_exercice_meta_bin_get_instance_private( OFA_EXERCICE_META_BIN( instance ));
+
+	g_free( priv->settings_prefix );
+
+	/* chain up to the parent class */
+	G_OBJECT_CLASS( ofa_exercice_meta_bin_parent_class )->finalize( instance );
+}
+
+static void
+exercice_meta_bin_dispose( GObject *instance )
+{
+	ofaExerciceMetaBinPrivate *priv;
+
+	g_return_if_fail( instance && OFA_IS_EXERCICE_META_BIN( instance ));
+
+	priv = ofa_exercice_meta_bin_get_instance_private( OFA_EXERCICE_META_BIN( instance ));
+
+	if( !priv->dispose_has_run ){
+
+		priv->dispose_has_run = TRUE;
+
+		/* unref object members here */
+		g_clear_object( &priv->group0 );
+	}
+
+	/* chain up to the parent class */
+	G_OBJECT_CLASS( ofa_exercice_meta_bin_parent_class )->dispose( instance );
+}
+
+static void
+ofa_exercice_meta_bin_init( ofaExerciceMetaBin *self )
+{
+	static const gchar *thisfn = "ofa_exercice_meta_bin_instance_init";
+	ofaExerciceMetaBinPrivate *priv;
+
+	g_debug( "%s: self=%p (%s)",
+			thisfn, ( void * ) self, G_OBJECT_TYPE_NAME( self ));
+
+	g_return_if_fail( self && OFA_IS_EXERCICE_META_BIN( self ));
+
+	priv = ofa_exercice_meta_bin_get_instance_private( self );
+
+	priv->dispose_has_run = FALSE;
+	priv->settings_prefix = g_strdup( G_OBJECT_TYPE_NAME( self ));
+	my_date_clear( &priv->begin );
+	my_date_clear( &priv->end );
+}
+
+static void
+ofa_exercice_meta_bin_class_init( ofaExerciceMetaBinClass *klass )
+{
+	static const gchar *thisfn = "ofa_exercice_meta_bin_class_init";
+
+	g_debug( "%s: klass=%p", thisfn, ( void * ) klass );
+
+	G_OBJECT_CLASS( klass )->dispose = exercice_meta_bin_dispose;
+	G_OBJECT_CLASS( klass )->finalize = exercice_meta_bin_finalize;
+
+	/**
+	 * ofaExerciceMetaBin::ofa-changed:
+	 *
+	 * This signal is sent on the #ofaExerciceMetaBin when any of the
+	 * underlying information is changed. This includes the exercice
+	 * name and the DBMS provider.
+	 *
+	 * There is no argument.
+	 *
+	 * Handler is of type:
+	 * void ( *handler )( ofaExerciceMetaBin *bin,
+	 * 						gpointer          user_data );
+	 */
+	st_signals[ CHANGED ] = g_signal_new_class_handler(
+				"ofa-changed",
+				OFA_TYPE_EXERCICE_META_BIN,
+				G_SIGNAL_RUN_LAST,
+				NULL,
+				NULL,								/* accumulator */
+				NULL,								/* accumulator data */
+				NULL,
+				G_TYPE_NONE,
+				0,
+				G_TYPE_NONE );
+}
+
+/**
+ * ofa_exercice_meta_bin_new:
+ * @hub: the #ofaHub object of the application.
+ * @settings_prefix: the prefix of the key in user settings.
+ * @rule: the usage of this widget.
+ *
+ * Returns: a newly defined composite widget which aggregates exercice
+ * meta datas: name and DBMS provider.
+ */
+ofaExerciceMetaBin *
+ofa_exercice_meta_bin_new( ofaHub *hub, const gchar *settings_prefix, guint rule )
+{
+	static const gchar *thisfn = "ofa_exercice_meta_bin_new";
+	ofaExerciceMetaBin *self;
+	ofaExerciceMetaBinPrivate *priv;
+
+	g_debug( "%s: hub=%p, settings_prefix=%s, guint=%u",
+			thisfn, ( void * ) hub, settings_prefix, rule );
+
+	g_return_val_if_fail( hub && OFA_IS_HUB( hub ), NULL );
+	g_return_val_if_fail( my_strlen( settings_prefix ), NULL );
+
+	self = g_object_new( OFA_TYPE_EXERCICE_META_BIN, NULL );
+
+	priv = ofa_exercice_meta_bin_get_instance_private( self );
+
+	priv->hub = hub;
+	priv->rule = rule;
+
+	g_free( priv->settings_prefix );
+	priv->settings_prefix = g_strdup( settings_prefix );
+
+	setup_bin( self );
+
+	return( self );
+}
+
+static void
+setup_bin( ofaExerciceMetaBin *self )
+{
+	ofaExerciceMetaBinPrivate *priv;
+	GtkBuilder *builder;
+	GObject *object;
+	GtkWidget *toplevel, *entry, *label, *btn;
+
+	priv = ofa_exercice_meta_bin_get_instance_private( self );
+
+	builder = gtk_builder_new_from_resource( st_resource_ui );
+
+	object = gtk_builder_get_object( builder, "emb-col0-hsize" );
+	g_return_if_fail( object && GTK_IS_SIZE_GROUP( object ));
+	priv->group0 = GTK_SIZE_GROUP( g_object_ref( object ));
+
+	object = gtk_builder_get_object( builder, "emb-window" );
+	g_return_if_fail( object && GTK_IS_WINDOW( object ));
+	toplevel = GTK_WIDGET( g_object_ref( object ));
+
+	my_utils_container_attach_from_window( GTK_CONTAINER( self ), GTK_WINDOW( toplevel ), "top" );
+
+	/* beginning date */
+	entry = my_utils_container_get_child_by_name( GTK_CONTAINER( self ), "emb-begin-entry" );
+	g_return_if_fail( entry && GTK_IS_ENTRY( entry ));
+	g_signal_connect( entry, "changed", G_CALLBACK( on_begin_changed ), self );
+	label = my_utils_container_get_child_by_name( GTK_CONTAINER( self ), "emb-begin-label" );
+	g_return_if_fail( label && GTK_IS_LABEL( label ));
+	my_date_editable_init( GTK_EDITABLE( entry ));
+	my_date_editable_set_format( GTK_EDITABLE( entry ), ofa_prefs_date_display( priv->hub ));
+	my_date_editable_set_label( GTK_EDITABLE( entry ), label, ofa_prefs_date_check( priv->hub ));
+	my_date_editable_set_date( GTK_EDITABLE( entry ), &priv->begin );
+	label = my_utils_container_get_child_by_name( GTK_CONTAINER( self ), "emb-begin-prompt" );
+	g_return_if_fail( label && GTK_IS_LABEL( label ));
+	gtk_label_set_mnemonic_widget( GTK_LABEL( label ), entry );
+
+	/* ending date */
+	entry = my_utils_container_get_child_by_name( GTK_CONTAINER( self ), "emb-end-entry" );
+	g_return_if_fail( entry && GTK_IS_ENTRY( entry ));
+	g_signal_connect( entry, "changed", G_CALLBACK( on_end_changed ), self );
+	label = my_utils_container_get_child_by_name( GTK_CONTAINER( self ), "emb-end-label" );
+	g_return_if_fail( label && GTK_IS_LABEL( label ));
+	my_date_editable_init( GTK_EDITABLE( entry ));
+	my_date_editable_set_format( GTK_EDITABLE( entry ), ofa_prefs_date_display( priv->hub ));
+	my_date_editable_set_label( GTK_EDITABLE( entry ), label, ofa_prefs_date_check( priv->hub ));
+	my_date_editable_set_date( GTK_EDITABLE( entry ), &priv->end );
+	label = my_utils_container_get_child_by_name( GTK_CONTAINER( self ), "emb-end-prompt" );
+	g_return_if_fail( label && GTK_IS_LABEL( label ));
+	gtk_label_set_mnemonic_widget( GTK_LABEL( label ), entry );
+
+	/* current flag */
+	btn = my_utils_container_get_child_by_name( GTK_CONTAINER( self ), "emb-current-btn" );
+	g_return_if_fail( btn && GTK_IS_CHECK_BUTTON( btn ));
+	g_signal_connect( btn, "toggled", G_CALLBACK( on_current_toggled ), self );
+
+	gtk_widget_destroy( toplevel );
+	g_object_unref( builder );
+}
+
+/**
+ * ofa_exercice_meta_bin_get_size_group:
+ * @bin: this #ofaExerciceMetaBin instance.
+ * @column: the desire column.
+ *
+ * Returns: the #GtkSizeGroup which handles the desired @column.
+ */
+GtkSizeGroup *
+ofa_exercice_meta_bin_get_size_group( ofaExerciceMetaBin *bin, guint column )
+{
+	static const gchar *thisfn = "ofa_exercice_meta_bin_get_size_group";
+	ofaExerciceMetaBinPrivate *priv;
+
+	g_return_val_if_fail( bin && OFA_IS_EXERCICE_META_BIN( bin ), NULL );
+
+	priv = ofa_exercice_meta_bin_get_instance_private( bin );
+
+	g_return_val_if_fail( !priv->dispose_has_run, NULL );
+
+	if( column == 0 ){
+		return( priv->group0 );
+	}
+
+	g_warning( "%s: unmanaged column=%u", thisfn, column );
+	return( NULL );
+}
+
+static void
+on_begin_changed( GtkEditable *editable, ofaExerciceMetaBin *self )
+{
+#if 0
+	ofaExerciceMetaBinPrivate *priv;
+
+	priv = ofa_exercice_meta_bin_get_instance_private( self );
+
+	//ofa_exercice_meta_set_begin_date( priv->exercice_meta, &priv->begin );
+#endif
+
+	changed_composite( self );
+}
+
+static void
+on_end_changed( GtkEditable *editable, ofaExerciceMetaBin *self )
+{
+#if 0
+	ofaExerciceMetaBinPrivate *priv;
+
+	priv = ofa_exercice_meta_bin_get_instance_private( self );
+
+	//ofa_exercice_meta_set_end_date( priv->exercice_meta, &priv->end );
+#endif
+
+	changed_composite( self );
+}
+
+static void
+on_current_toggled( GtkToggleButton *button, ofaExerciceMetaBin *self )
+{
+#if 0
+	ofaExerciceMetaBinPrivate *priv;
+
+	priv = ofa_exercice_meta_bin_get_instance_private( self );
+
+	//ofa_exercice_meta_set_is_current( priv->exercice_meta, gtk_toggle_button_get_active( button ));
+#endif
+
+	changed_composite( self );
+}
+
+static void
+changed_composite( ofaExerciceMetaBin *self )
+{
+#if 0
+	ofaExerciceMetaBinPrivate *priv;
+
+	priv = ofa_exercice_meta_bin_get_instance_private( self );
+#endif
+
+	g_signal_emit_by_name( self, "ofa-changed" );
+}
+
+/**
+ * ofa_exercice_meta_bin_is_valid:
+ * @bin: this #ofaExerciceMetaBin instance.
+ * @error_message: [allow-none]: the error message to be displayed.
+ *
+ * The dialog is always valid.
+ */
+gboolean
+ofa_exercice_meta_bin_is_valid( ofaExerciceMetaBin *bin, gchar **error_message )
+{
+	return( TRUE );
+}
+
+/**
+ * ofa_exercice_meta_bin_apply:
+ * @bin: this #ofaExerciceMetaBin instance.
+ *
+ * Returns: %TRUE if the exercice has been successfully defined in the
+ * settings.
+ */
+gboolean
+ofa_exercice_meta_bin_apply( ofaExerciceMetaBin *bin )
+{
+#if 0
+	ofaExerciceMetaBinPrivate *priv;
+	ofaIDBProvider *provider;
+	ofaIDBDossierMeta *meta;
+	ofaDossierCollection *collection;
+
+	g_return_val_if_fail( bin && OFA_IS_EXERCICE_META_BIN( bin ), FALSE );
+	g_return_val_if_fail( ofa_exercice_meta_bin_get_valid( bin, NULL ), FALSE );
+
+	priv = ofa_exercice_meta_bin_get_instance_private( bin );
+
+	g_return_val_if_fail( !priv->dispose_has_run, NULL );
+
+	if( !priv->exercice_meta ){
+		priv->exercice_meta = ofa_exercice_meta_new();
+	}
+
+	ofa_exercice_meta_set_exercice_name( priv->exercice_meta, priv->exercice_name );
+	ofa_exercice_meta_set_dbprovider( priv->exercice_meta, priv->provider );
+
+	collection = ofa_hub_get_dossier_collection( priv->hub );
+	g_return_val_if_fail( collection && OFA_IS_DOSSIER_COLLECTION( collection ), FALSE );
+	ofa_dossier_collection_set_exercice_meta( collection, priv->exercice_meta );
+
+	return( meta );
+#endif
+	return( TRUE );
+}
+
+#if 0
+/**
+ * ofa_exercice_meta_bin_get_exercice_meta:
+ * @bin: this #ofaExerciceMetaBin instance.
+ *
+ * Returns: the #ofaExerciceMeta object.
+ */
+ofaExerciceMeta *
+ofa_exercice_meta_bin_get_exercice_meta( ofaExerciceMetaBin *bin )
+{
+	ofaExerciceMetaBinPrivate *priv;
+
+	g_return_val_if_fail( bin && OFA_IS_EXERCICE_META_BIN( bin ), NULL );
+
+	priv = ofa_exercice_meta_bin_get_instance_private( bin );
+
+	g_return_val_if_fail( !priv->dispose_has_run, NULL );
+
+	return( priv->exercice_meta );
+}
+
+/**
+ * ofa_exercice_meta_bin_set_exercice_meta:
+ * @bin: this #ofaExerciceMetaBin instance.
+ * @exercice_meta: the #ofaExerciceMeta object to be set.
+ *
+ * Set the #ofaExerciceMeta object.
+ */
+void
+ofa_exercice_meta_bin_set_exercice_meta( ofaExerciceMetaBin *bin, ofaExerciceMeta *exercice_meta )
+{
+	ofaExerciceMetaBinPrivate *priv;
+
+	g_return_if_fail( bin && OFA_IS_EXERCICE_META_BIN( bin ));
+
+	priv = ofa_exercice_meta_bin_get_instance_private( bin );
+
+	g_return_if_fail( !priv->dispose_has_run );
+
+	g_clear_object( &priv->exercice_meta );
+	priv->exercice_meta = exercice_meta ? g_object_ref( exercice_meta ) : NULL;
+}
+#endif
---
+++ src/ui/ofa-exercice-meta-bin.h
@@ -0,0 +1,90 @@
+/*
+ * Open Firm Accounting
+ * A double-entry accounting application for professional services.
+ *
+ * Copyright (C) 2014,2015,2016 Pierre Wieser (see AUTHORS)
+ *
+ * Open Firm Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Firm Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Firm Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ */
+
+#ifndef __OFA_EXERCICE_META_BIN_H__
+#define __OFA_EXERCICE_META_BIN_H__
+
+/**
+ * SECTION: ofa_exercice_meta_bin
+ * @short_description: #ofaExerciceMetaBin class definition.
+ * @include: ui/ofa-exercice-meta-bin.h
+ *
+ * Let the user define a new exercice.
+ *
+ * Development rules:
+ * - type:       bin (parent='top')
+ * - validation: yes (has 'ofa-changed' signal)
+ * - settings:   no
+ * - current:    no
+ */
+
+#include <gtk/gtk.h>
+
+#include "api/ofa-hub-def.h"
+
+G_BEGIN_DECLS
+
+#define OFA_TYPE_EXERCICE_META_BIN                ( ofa_exercice_meta_bin_get_type())
+#define OFA_EXERCICE_META_BIN( object )           ( G_TYPE_CHECK_INSTANCE_CAST( object, OFA_TYPE_EXERCICE_META_BIN, ofaExerciceMetaBin ))
+#define OFA_EXERCICE_META_BIN_CLASS( klass )      ( G_TYPE_CHECK_CLASS_CAST( klass, OFA_TYPE_EXERCICE_META_BIN, ofaExerciceMetaBinClass ))
+#define OFA_IS_EXERCICE_META_BIN( object )        ( G_TYPE_CHECK_INSTANCE_TYPE( object, OFA_TYPE_EXERCICE_META_BIN ))
+#define OFA_IS_EXERCICE_META_BIN_CLASS( klass )   ( G_TYPE_CHECK_CLASS_TYPE(( klass ), OFA_TYPE_EXERCICE_META_BIN ))
+#define OFA_EXERCICE_META_BIN_GET_CLASS( object ) ( G_TYPE_INSTANCE_GET_CLASS(( object ), OFA_TYPE_EXERCICE_META_BIN, ofaExerciceMetaBinClass ))
+
+typedef struct {
+	/*< public members >*/
+	GtkBin      parent;
+}
+	ofaExerciceMetaBin;
+
+typedef struct {
+	/*< public members >*/
+	GtkBinClass parent;
+}
+	ofaExerciceMetaBinClass;
+
+GType               ofa_exercice_meta_bin_get_type         ( void ) G_GNUC_CONST;
+
+ofaExerciceMetaBin *ofa_exercice_meta_bin_new              ( ofaHub *hub,
+																	const gchar *settings_prefix,
+																	guint rule);
+
+GtkSizeGroup       *ofa_exercice_meta_bin_get_size_group   ( ofaExerciceMetaBin *bin,
+																	guint column );
+
+gboolean            ofa_exercice_meta_bin_is_valid         ( ofaExerciceMetaBin *bin,
+																	gchar **error_message );
+
+gboolean            ofa_exercice_meta_bin_apply            ( ofaExerciceMetaBin *bin );
+
+#if 0
+ofaExerciceMeta    *ofa_exercice_meta_bin_get_exercice_meta( ofaExerciceMetaBin *bin );
+
+void                ofa_exercice_meta_bin_set_exercice_meta( ofaExerciceMetaBin *bin,
+																	ofaExerciceMeta *exercice_meta );
+#endif
+
+G_END_DECLS
+
+#endif /* __OFA_EXERCICE_META_BIN_H__ */
