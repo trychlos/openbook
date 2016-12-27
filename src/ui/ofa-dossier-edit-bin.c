33fe21fe8ecc80dd6d70d3a84568f084cbedb65b
Pierre Wieser <pwieser@trychlos.org>
2016-12-27 12:11:52 +0100

    ofaDossierEditBin: new class

---
+++ src/ui/ofa-dossier-edit-bin.c
@@ -0,0 +1,453 @@
+/*
+ * Open Firm Accounting
+ * A double-entry accounting application for professional services.
+ *
+ * Copyright (C) 2014,2015,2016 Pierre Wieser (see AUTHORS)
+ *
+ * Open Firm Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Firm Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Firm Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <glib/gi18n.h>
+
+#include "my/my-utils.h"
+
+#include "api/ofa-dossier-meta.h"
+#include "api/ofa-exercice-meta.h"
+#include "api/ofa-hub.h"
+#include "api/ofa-idbdossier-editor.h"
+#include "api/ofa-idbexercice-editor.h"
+#include "api/ofa-idbprovider.h"
+#include "api/ofa-igetter.h"
+
+#include "core/ofa-admin-credentials-bin.h"
+
+#include "ui/ofa-dossier-edit-bin.h"
+#include "ui/ofa-dossier-meta-bin.h"
+#include "ui/ofa-exercice-meta-bin.h"
+
+/* private instance data
+ */
+typedef struct {
+	gboolean                dispose_has_run;
+
+	/* initialization
+	 */
+	ofaHub                 *hub;
+	gchar                  *settings_prefix;
+	guint                   rule;
+
+	/* UI
+	 */
+	ofaDossierMetaBin      *dossier_meta_bin;
+	GtkWidget              *dossier_editor_parent;
+	ofaIDBDossierEditor    *dossier_editor_bin;
+	ofaExerciceMetaBin     *exercice_meta_bin;
+	GtkWidget              *exercice_editor_parent;
+	ofaIDBExerciceEditor   *exercice_editor_bin;
+	ofaAdminCredentialsBin *admin_bin;
+	GtkWidget              *msg_label;
+
+	/* runtime
+	 */
+	ofaIDBProvider         *provider;
+	ofaDossierMeta         *dossier_meta;
+	ofaExerciceMeta        *exercice_meta;
+}
+	ofaDossierEditBinPrivate;
+
+/* signals defined here
+ */
+enum {
+	CHANGED = 0,
+	N_SIGNALS
+};
+
+static guint st_signals[ N_SIGNALS ]    = { 0 };
+
+static const gchar *st_resource_ui      = "/org/trychlos/openbook/ui/ofa-dossier-edit-bin.ui";
+
+static void setup_bin( ofaDossierEditBin *self );
+static void on_dossier_meta_changed( ofaDossierMetaBin *bin, ofaDossierEditBin *self );
+static void on_exercice_meta_changed( ofaExerciceMetaBin *bin, ofaDossierEditBin *self );
+static void on_admin_credentials_changed( ofaAdminCredentialsBin *bin, const gchar *account, const gchar *password, ofaDossierEditBin *self );
+//static void on_actions_bin_changed( ofaDossierActionsBin *bin, ofaDossierEditBin *self );
+static void changed_composite( ofaDossierEditBin *self );
+
+G_DEFINE_TYPE_EXTENDED( ofaDossierEditBin, ofa_dossier_edit_bin, GTK_TYPE_BIN, 0,
+		G_ADD_PRIVATE( ofaDossierEditBin ))
+
+static void
+dossier_edit_bin_finalize( GObject *instance )
+{
+	static const gchar *thisfn = "ofa_dossier_edit_bin_finalize";
+	ofaDossierEditBinPrivate *priv;
+
+	g_debug( "%s: instance=%p (%s)",
+			thisfn, ( void * ) instance, G_OBJECT_TYPE_NAME( instance ));
+
+	g_return_if_fail( instance && OFA_IS_DOSSIER_EDIT_BIN( instance ));
+
+	/* free data members here */
+	priv = ofa_dossier_edit_bin_get_instance_private( OFA_DOSSIER_EDIT_BIN( instance ));
+
+	g_free( priv->settings_prefix );
+
+	/* chain up to the parent class */
+	G_OBJECT_CLASS( ofa_dossier_edit_bin_parent_class )->finalize( instance );
+}
+
+static void
+dossier_edit_bin_dispose( GObject *instance )
+{
+	ofaDossierEditBinPrivate *priv;
+
+	g_return_if_fail( instance && OFA_IS_DOSSIER_EDIT_BIN( instance ));
+
+	priv = ofa_dossier_edit_bin_get_instance_private( OFA_DOSSIER_EDIT_BIN( instance ));
+
+	if( !priv->dispose_has_run ){
+
+		priv->dispose_has_run = TRUE;
+
+		/* unref object members here */
+	}
+
+	/* chain up to the parent class */
+	G_OBJECT_CLASS( ofa_dossier_edit_bin_parent_class )->dispose( instance );
+}
+
+static void
+ofa_dossier_edit_bin_init( ofaDossierEditBin *self )
+{
+	static const gchar *thisfn = "ofa_dossier_edit_bin_instance_init";
+	ofaDossierEditBinPrivate *priv;
+
+	g_debug( "%s: self=%p (%s)",
+			thisfn, ( void * ) self, G_OBJECT_TYPE_NAME( self ));
+
+	g_return_if_fail( self && OFA_IS_DOSSIER_EDIT_BIN( self ));
+
+	priv = ofa_dossier_edit_bin_get_instance_private( self );
+
+	priv->dispose_has_run = FALSE;
+	priv->settings_prefix = g_strdup( G_OBJECT_TYPE_NAME( self ));
+}
+
+static void
+ofa_dossier_edit_bin_class_init( ofaDossierEditBinClass *klass )
+{
+	static const gchar *thisfn = "ofa_dossier_edit_bin_class_init";
+
+	g_debug( "%s: klass=%p", thisfn, ( void * ) klass );
+
+	G_OBJECT_CLASS( klass )->dispose = dossier_edit_bin_dispose;
+	G_OBJECT_CLASS( klass )->finalize = dossier_edit_bin_finalize;
+
+	/**
+	 * ofaDossierEditBin::ofa-changed:
+	 *
+	 * This signal is sent on the #ofaDossierEditBin when any of the
+	 * underlying information is changed. This includes the dossier
+	 * name, the DBMS provider, the connection informations and the
+	 * DBMS root credentials
+	 *
+	 * There is no argument.
+	 *
+	 * Handler is of type:
+	 * void ( *handler )( ofaDossierEditBin *bin,
+	 * 						gpointer         user_data );
+	 */
+	st_signals[ CHANGED ] = g_signal_new_class_handler(
+				"ofa-changed",
+				OFA_TYPE_DOSSIER_EDIT_BIN,
+				G_SIGNAL_RUN_LAST,
+				NULL,
+				NULL,								/* accumulator */
+				NULL,								/* accumulator data */
+				NULL,
+				G_TYPE_NONE,
+				0,
+				G_TYPE_NONE );
+}
+
+/**
+ * ofa_dossier_edit_bin_new:
+ * @hub: the #ofaHub object of the application.
+ * @settings_prefix: the prefix of the key in user settings.
+ * @rule: the usage of this widget.
+ *
+ * Returns: a newly defined composite widget.
+ */
+ofaDossierEditBin *
+ofa_dossier_edit_bin_new( ofaHub *hub, const gchar *settings_prefix, guint rule )
+{
+	static const gchar *thisfn = "ofa_dossier_edit_bin_new";
+	ofaDossierEditBin *self;
+	ofaDossierEditBinPrivate *priv;
+
+	g_debug( "%s: hub=%p, settings_prefix=%s, guint=%u",
+			thisfn, ( void * ) hub, settings_prefix, rule );
+
+	g_return_val_if_fail( hub && OFA_IS_HUB( hub ), NULL );
+	g_return_val_if_fail( my_strlen( settings_prefix ), NULL );
+
+	self = g_object_new( OFA_TYPE_DOSSIER_EDIT_BIN, NULL );
+
+	priv = ofa_dossier_edit_bin_get_instance_private( self );
+
+	priv->hub = hub;
+	priv->rule = rule;
+
+	g_free( priv->settings_prefix );
+	priv->settings_prefix = g_strdup( settings_prefix );
+
+	setup_bin( self );
+
+	return( self );
+}
+
+static void
+setup_bin( ofaDossierEditBin *self )
+{
+	ofaDossierEditBinPrivate *priv;
+	GtkBuilder *builder;
+	GObject *object;
+	GtkSizeGroup *colgroup;
+	GtkWidget *toplevel, *parent;
+
+	priv = ofa_dossier_edit_bin_get_instance_private( self );
+
+	colgroup = gtk_size_group_new( GTK_SIZE_GROUP_HORIZONTAL );
+	builder = gtk_builder_new_from_resource( st_resource_ui );
+
+	object = gtk_builder_get_object( builder, "deb-window" );
+	g_return_if_fail( object && GTK_IS_WINDOW( object ));
+	toplevel = GTK_WIDGET( g_object_ref( object ));
+
+	my_utils_container_attach_from_window( GTK_CONTAINER( self ), GTK_WINDOW( toplevel ), "top" );
+
+	/* dossier meta datas */
+	parent = my_utils_container_get_child_by_name( GTK_CONTAINER( self ), "deb-dossier-meta-parent" );
+	g_return_if_fail( parent && GTK_IS_CONTAINER( parent ));
+	priv->dossier_meta_bin = ofa_dossier_meta_bin_new( priv->hub, priv->settings_prefix, priv->rule );
+	g_signal_connect( priv->dossier_meta_bin, "ofa-changed", G_CALLBACK( on_dossier_meta_changed ), self );
+	gtk_container_add( GTK_CONTAINER( parent ), GTK_WIDGET( priv->dossier_meta_bin ));
+	my_utils_size_group_add_size_group( colgroup, ofa_dossier_meta_bin_get_size_group( priv->dossier_meta_bin, 0 ));
+
+	/* dossier dbeditor */
+	parent = my_utils_container_get_child_by_name( GTK_CONTAINER( self ), "deb-dossier-editor-parent" );
+	g_return_if_fail( parent && GTK_IS_CONTAINER( parent ));
+	priv->dossier_editor_parent = parent;
+
+	/* exercice meta datas */
+	parent = my_utils_container_get_child_by_name( GTK_CONTAINER( self ), "deb-exercice-meta-parent" );
+	g_return_if_fail( parent && GTK_IS_CONTAINER( parent ));
+	priv->exercice_meta_bin = ofa_exercice_meta_bin_new( priv->hub, priv->settings_prefix, priv->rule );
+	g_signal_connect( priv->exercice_meta_bin, "ofa-changed", G_CALLBACK( on_exercice_meta_changed ), self );
+	gtk_container_add( GTK_CONTAINER( parent ), GTK_WIDGET( priv->exercice_meta_bin ));
+	my_utils_size_group_add_size_group( colgroup, ofa_exercice_meta_bin_get_size_group( priv->exercice_meta_bin, 0 ));
+
+	/* exercice dbeditor */
+	parent = my_utils_container_get_child_by_name( GTK_CONTAINER( self ), "deb-exercice-editor-parent" );
+	g_return_if_fail( parent && GTK_IS_CONTAINER( parent ));
+	priv->exercice_editor_parent = parent;
+
+	/* administrative credentials */
+	parent = my_utils_container_get_child_by_name( GTK_CONTAINER( self ), "deb-admin-parent" );
+	g_return_if_fail( parent && GTK_IS_CONTAINER( parent ));
+	priv->admin_bin = ofa_admin_credentials_bin_new();
+	g_signal_connect( priv->admin_bin, "ofa-changed", G_CALLBACK( on_admin_credentials_changed ), self );
+	gtk_container_add( GTK_CONTAINER( parent ), GTK_WIDGET( priv->admin_bin ));
+	my_utils_size_group_add_size_group( colgroup, ofa_admin_credentials_bin_get_size_group( priv->admin_bin, 0 ));
+
+#if 0
+	/* actions on open */
+	parent = my_utils_container_get_child_by_name( GTK_CONTAINER( self ), "deb-actions-parent" );
+	g_return_if_fail( parent && GTK_IS_CONTAINER( parent ));
+	priv->actions_bin = ofa_dossier_actions_bin_new();
+	g_signal_connect( priv->actions_bin, "ofa-changed", G_CALLBACK( on_actions_bin_changed ), self );
+	gtk_container_add( GTK_CONTAINER( parent ), GTK_WIDGET( priv->actions_bin ));
+#endif
+
+	gtk_widget_destroy( toplevel );
+	g_object_unref( builder );
+	g_object_unref( colgroup );
+}
+
+static void
+on_dossier_meta_changed( ofaDossierMetaBin *bin, ofaDossierEditBin *self )
+{
+#if 0
+	ofaDossierEditBinPrivate *priv;
+	GtkSizeGroup *colgroup;
+
+	priv = ofa_dossier_edit_bin_get_instance_private( self );
+
+	priv->dossier_meta = dossier_meta;
+	if( provider != priv->provider ){
+		priv->provider = provider;
+		colgroup = gtk_size_group_new( GTK_SIZE_GROUP_HORIZONTAL );
+		/* dossier editor */
+		if( priv->dossier_editor_bin ){
+			gtk_container_remove( GTK_CONTAINER( priv->dossier_editor_parent ), GTK_WIDGET( priv->dossier_editor_bin ));
+		}
+		priv->dossier_editor_bin = ofa_idbprovider_new_dossier_editor( provider, priv->editable );
+		gtk_container_add( GTK_CONTAINER( priv->dossier_editor_parent ), GTK_WIDGET( priv->dossier_editor_bin ));
+		my_utils_size_group_add_size_group( colgroup, ofa_idbdossier_editor_get_size_group( priv->dossier_editor_bin, 0 ));
+		/* exercice editor */
+		if( priv->exercice_editor_bin ){
+			gtk_container_remove( GTK_CONTAINER( priv->exercice_editor_parent ), GTK_WIDGET( priv->exercice_editor_bin ));
+		}
+		priv->exercice_editor_bin = ofa_idbprovider_new_exercice_editor( provider, priv->editable );
+		gtk_container_add( GTK_CONTAINER( priv->exercice_editor_parent ), GTK_WIDGET( priv->exercice_editor_bin ));
+		my_utils_size_group_add_size_group( colgroup, ofa_idbexercice_editor_get_size_group( priv->exercice_editor_bin, 0 ));
+		gtk_widget_show_all( GTK_WIDGET( self ));
+	}
+
+	changed_composite( self );
+#endif
+}
+
+static void
+on_exercice_meta_changed( ofaExerciceMetaBin *bin, ofaDossierEditBin *self )
+{
+#if 0
+	ofaDossierEditBinPrivate *priv;
+
+	priv = ofa_dossier_edit_bin_get_instance_private( self );
+
+	priv->exercice_meta = exercice_meta;
+
+	changed_composite( self );
+#endif
+}
+
+static void
+on_admin_credentials_changed( ofaAdminCredentialsBin *bin, const gchar *account, const gchar *password, ofaDossierEditBin *self )
+{
+	changed_composite( self );
+}
+
+#if 0
+static void
+on_actions_bin_changed( ofaDossierActionsBin *bin, ofaDossierEditBin *self )
+{
+	changed_composite( self );
+}
+#endif
+
+static void
+changed_composite( ofaDossierEditBin *self )
+{
+	g_signal_emit_by_name( self, "ofa-changed" );
+}
+
+/**
+ * ofa_dossier_edit_bin_is_valid:
+ * @bin: this #ofaDossierEditBin instance.
+ * @message: [out][allow-none]: a placeholder for the output error message.
+ *
+ * Returns: %TRUE if the dialog is valid.
+ */
+gboolean
+ofa_dossier_edit_bin_is_valid( ofaDossierEditBin *bin, gchar **message )
+{
+	ofaDossierEditBinPrivate *priv;
+	gboolean ok = TRUE;
+
+	g_return_val_if_fail( bin && OFA_IS_DOSSIER_EDIT_BIN( bin ), FALSE );
+
+	priv = ofa_dossier_edit_bin_get_instance_private( bin );
+
+	g_return_val_if_fail( !priv->dispose_has_run, FALSE );
+
+	if( ok ){
+		ok = ofa_dossier_meta_bin_is_valid( priv->dossier_meta_bin, message );
+	}
+	/*
+	if( ok ){
+		ok = priv->dossier_editor_bin ? ofa_idbdossier_editor_is_valid( priv->dossier_editor_bin, message ) : TRUE;
+	}
+	*/
+	if( ok ){
+		ok = ofa_exercice_meta_bin_is_valid( priv->exercice_meta_bin, message );
+	}
+	/*
+	if( ok ){
+		ok = priv->exercice_editor_bin ? ofa_idbexercice_editor_is_valid( priv->exercice_editor_bin, message ) : TRUE;
+	}
+	*/
+	if( ok ){
+		ok = ofa_admin_credentials_bin_is_valid( priv->admin_bin, message );
+	}
+#if 0
+	if( ok ){
+		ok = ofa_dossier_actions_bin_is_valid( priv->actions_bin, message );
+	}
+#endif
+
+	return( ok );
+}
+
+/**
+ * ofa_dossier_edit_bin_apply:
+ *
+ * Define the dossier in user settings.
+ *
+ * Returns: %TRUE if the new dossier has been successfully registered.
+ */
+gboolean
+ofa_dossier_edit_bin_apply( ofaDossierEditBin *bin )
+{
+	ofaDossierEditBinPrivate *priv;
+	gboolean ok = TRUE;
+
+	g_return_val_if_fail( bin && OFA_IS_DOSSIER_EDIT_BIN( bin ), FALSE );
+
+	priv = ofa_dossier_edit_bin_get_instance_private( bin );
+
+	g_return_val_if_fail( !priv->dispose_has_run, FALSE );
+
+	if( ok ){
+		ok = ofa_dossier_meta_bin_apply( priv->dossier_meta_bin );
+	}
+	/*
+	if( ok ){
+		ok = priv->dossier_editor_bin ? ofa_idbdossier_editor_apply( priv->dossier_editor_bin ) : TRUE;
+	}
+	*/
+	if( ok ){
+		ok = ofa_exercice_meta_bin_apply( priv->exercice_meta_bin );
+	}
+	/*
+	if( ok ){
+		ok = priv->exercice_editor_bin ? ofa_idbexercice_editor_apply( priv->exercice_editor_bin ) : TRUE;
+	}
+	*/
+#if 0
+	if( ok ){
+		ok = ofa_dossier_actions_bin_apply( priv->actions_bin );
+	}
+#endif
+
+	return( ok );
+}
---
+++ src/ui/ofa-dossier-edit-bin.h
@@ -0,0 +1,105 @@
+/*
+ * Open Firm Accounting
+ * A double-entry accounting application for professional services.
+ *
+ * Copyright (C) 2014,2015,2016 Pierre Wieser (see AUTHORS)
+ *
+ * Open Firm Accounting is free software; you can redistribute it
+ * and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * Open Firm Accounting is distributed in the hope that it will be
+ * useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Open Firm Accounting; see the file COPYING. If not,
+ * see <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *   Pierre Wieser <pwieser@trychlos.org>
+ */
+
+#ifndef __OFA_DOSSIER_EDIT_BIN_H__
+#define __OFA_DOSSIER_EDIT_BIN_H__
+
+/**
+ * SECTION: ofa_dossier_edit_bin
+ * @short_description: #ofaDossierEditBin class definition.
+ * @include: ui/ofa-dossier-edit-bin.h
+ *
+ * Let the user define a new dossier, selecting the DBMS provider and
+ * its connection properties, registering it in the settings.
+ *
+ * This is in particular the main component of the Dossier/New dialog
+ * box.
+ *
+ * This dialog is a composite of:
+ *
+ *   ofaDossierEditBin
+ *    |
+ *    +- ofaDossierMetaBin
+ *    |
+ *    +- ofaIDBDossierEditor
+ *    |
+ *    +- ofaExerciceMetaBin
+ *    |
+ *    +- ofaIDBExerciceEditor
+ *    |
+ *    +- ofaAdminCredentialsBin
+ *
+ * Development rules:
+ * - type:       bin (parent='top')
+ * - validation: yes (has 'ofa-changed' signal)
+ * - settings:   no
+ * - current:    no
+ */
+
+#include <gtk/gtk.h>
+
+#include "api/ofa-hub-def.h"
+
+G_BEGIN_DECLS
+
+#define OFA_TYPE_DOSSIER_EDIT_BIN                ( ofa_dossier_edit_bin_get_type())
+#define OFA_DOSSIER_EDIT_BIN( object )           ( G_TYPE_CHECK_INSTANCE_CAST( object, OFA_TYPE_DOSSIER_EDIT_BIN, ofaDossierEditBin ))
+#define OFA_DOSSIER_EDIT_BIN_CLASS( klass )      ( G_TYPE_CHECK_CLASS_CAST( klass, OFA_TYPE_DOSSIER_EDIT_BIN, ofaDossierEditBinClass ))
+#define OFA_IS_DOSSIER_EDIT_BIN( object )        ( G_TYPE_CHECK_INSTANCE_TYPE( object, OFA_TYPE_DOSSIER_EDIT_BIN ))
+#define OFA_IS_DOSSIER_EDIT_BIN_CLASS( klass )   ( G_TYPE_CHECK_CLASS_TYPE(( klass ), OFA_TYPE_DOSSIER_EDIT_BIN ))
+#define OFA_DOSSIER_EDIT_BIN_GET_CLASS( object ) ( G_TYPE_INSTANCE_GET_CLASS(( object ), OFA_TYPE_DOSSIER_EDIT_BIN, ofaDossierEditBinClass ))
+
+typedef struct {
+	/*< public members >*/
+	GtkBin      parent;
+}
+	ofaDossierEditBin;
+
+typedef struct {
+	/*< public members >*/
+	GtkBinClass parent;
+}
+	ofaDossierEditBinClass;
+
+/**
+ * Rules for #ofaDossierEditBin:
+ */
+enum {
+	DOSSIER_EDIT_NEW = 0,
+};
+
+GType              ofa_dossier_edit_bin_get_type( void ) G_GNUC_CONST;
+
+ofaDossierEditBin *ofa_dossier_edit_bin_new     ( ofaHub *hub,
+														const gchar *settings_prefix,
+														guint rule );
+
+gboolean           ofa_dossier_edit_bin_is_valid( ofaDossierEditBin *bin,
+														gchar **message );
+
+gboolean           ofa_dossier_edit_bin_apply   ( ofaDossierEditBin *bin );
+
+G_END_DECLS
+
+#endif /* __OFA_DOSSIER_EDIT_BIN_H__ */
