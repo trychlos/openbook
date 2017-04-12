9fcc64b1e263240b24fd993e8dcf09237e81ffa1
Pierre Wieser <pwieser@trychlos.org>
2017-04-12 15:36:02 +0200

    ofoClass: export child tables with a version number

--- src/core/ofo-class.c
+++ src/core/ofo-class.c
@@ -107,7 +107,8 @@ static const ofsBoxDef st_doc_defs[] = {
 		{ 0 }
 };
 
-#define TABLES_COUNT                    2
+#define CLASS_TABLES_COUNT              2
+#define CLASS_EXPORT_VERSION            1
 
 /* priv instance data
  */
@@ -882,7 +883,7 @@ iexportable_export_default( ofaIExportable *exportable )
 
 	count = ( gulong ) g_list_length( dataset );
 	if( ofa_stream_format_get_with_headers( stformat )){
-		count += TABLES_COUNT;
+		count += CLASS_TABLES_COUNT;
 	}
 	for( it=dataset ; it ; it=it->next ){
 		class = OFO_CLASS( it->data );
@@ -890,10 +891,18 @@ iexportable_export_default( ofaIExportable *exportable )
 	}
 	ofa_iexportable_set_count( exportable, count );
 
-	/* add new ofsBoxDef array at the end of the list */
-	ok = ofa_iexportable_append_headers( exportable,
-				TABLES_COUNT, st_boxed_defs, st_doc_defs );
+	/* add a version line at the very beginning of the file */
+	str1 = g_strdup_printf( "0%cVersion%c%u", field_sep, field_sep, CLASS_EXPORT_VERSION );
+	ok = ofa_iexportable_append_line( exportable, str1 );
+	g_free( str1 );
 
+	if( ok ){
+		/* add new ofsBoxDef array at the end of the list */
+		ok = ofa_iexportable_append_headers( exportable,
+					CLASS_TABLES_COUNT, st_boxed_defs, st_doc_defs );
+	}
+
+	/* export the dataset */
 	for( it=dataset ; it && ok ; it=it->next ){
 		class = OFO_CLASS( it->data );
 
