b2c54b9cbbb603c6be979e65e889193cffab299f
Pierre Wieser <pwieser@trychlos.org>
2018-06-11 21:51:55 +0200

    ofa_unzip: list embedded streams, dump each header and data stream

--- src/cmdline/ofa-unzip.c
+++ src/cmdline/ofa-unzip.c
@@ -20,9 +20,6 @@
  *
  * Authors:
  *   Pierre Wieser <pwieser@trychlos.org>
- *
- * To display debug messages, run the command:
- *   $ G_MESSAGES_DEBUG=OFA _install/bin/openbook
  */
 
 #ifdef HAVE_CONFIG_H
@@ -32,11 +29,30 @@
 #include <archive.h>
 #include <archive_entry.h>
 #include <glib.h>
+#include <glib/gi18n.h>
 #include <stdlib.h>
 
 #include "my/my-utils.h"
 
 #include "api/ofa-backup-header.h"
+
+static gchar   *st_data = "";						/* name of the data stream to be dumped */
+static gchar   *st_header = "";						/* name of the header stream to be dumped */
+static gboolean st_list = TRUE;						/* whether to list stream (default) */
+static gchar   *st_uri = "";						/* URI of the file */
+
+static GOptionEntry st_entries[] = {
+
+	{ "data",   'd', 0, G_OPTION_ARG_STRING,   &st_data,
+			N_( "Dump the specified data stream" ), "<name>" },
+	{ "header", 'h', 0, G_OPTION_ARG_STRING,   &st_header,
+			N_( "Dump the specified header stream" ), "<name>" },
+	{ "list",   'l', 0, G_OPTION_ARG_NONE,     &st_list,
+			N_( "List the embedded streams" ), NULL },
+	{ "uri",    'u', 0, G_OPTION_ARG_FILENAME, &st_uri,
+			N_( "The URI of the file to be examined" ), "<uri>" },
+	{ NULL }
+};
 
 /*
  * ofa-unzip is a small cmdline utility (no GUI) to extract the DAT member
@@ -48,6 +64,147 @@
 
 #define BUFSIZE 32768
 
+static gint
+list_archive( const gchar *basename, const gchar *uri )
+{
+	static const gchar *thisfn = "ofa_unzip_list_archive";
+	GFile *file;
+	gchar *pathname, *buffer;
+	struct archive *archive;
+	struct archive_entry *entry;
+	const gchar *cname;
+	gboolean found;
+
+	found = FALSE;
+	archive = archive_read_new();
+	archive_read_support_filter_all( archive );
+	archive_read_support_format_all( archive );
+
+	file = g_file_new_for_uri( uri );
+	pathname = g_file_get_path( file );
+	if( archive_read_open_filename( archive, pathname, 16384 ) != ARCHIVE_OK ){
+		g_warning( "%s archive_read_open_filename: path=%s, %s", thisfn, pathname, archive_error_string( archive ));
+
+	} else {
+		buffer = g_new0( gchar, BUFSIZE );
+		while( archive_read_next_header( archive, &entry ) == ARCHIVE_OK ){
+			found = TRUE;
+			cname = archive_entry_pathname( entry );
+			g_print( _( "[%s] found stream '%s'\n" ), basename, cname );
+			archive_read_data_skip( archive );
+		}
+		g_free( buffer );
+	}
+
+	g_free( pathname );
+	g_object_unref( file );
+	archive_read_close( archive );
+	archive_read_free( archive );
+
+	return( found ? 0 : 1 );
+}
+
+static gint
+dump_header( const gchar *basename, const gchar *uri, const gchar *name )
+{
+	static const gchar *thisfn = "ofa_unzip_dump_header";
+	GFile *file;
+	gchar *pathname, *buffer, *searched;
+	struct archive *archive;
+	struct archive_entry *entry;
+	const gchar *cname;
+	gboolean found;
+	gsize bytes_read;
+
+	found = FALSE;
+	archive = archive_read_new();
+	archive_read_support_filter_all( archive );
+	archive_read_support_format_all( archive );
+
+	file = g_file_new_for_uri( uri );
+	pathname = g_file_get_path( file );
+	if( archive_read_open_filename( archive, pathname, 16384 ) != ARCHIVE_OK ){
+		g_warning( "%s: archive_read_open_filename: path=%s, %s", thisfn, pathname, archive_error_string( archive ));
+
+	} else {
+		buffer = g_new0( gchar, BUFSIZE );
+		searched = g_strdup_printf( "%s%s", OFA_BACKUP_HEADER_HEADER, name );
+		while( archive_read_next_header( archive, &entry ) == ARCHIVE_OK ){
+			cname = archive_entry_pathname( entry );
+			if( !g_strcmp0( cname, searched )){
+				found = TRUE;
+				g_print( _( "[%s] dumping stream '%s':\n" ), basename, cname );
+				while(( bytes_read = archive_read_data( archive, buffer, BUFSIZE )) > 0 ){
+					g_print( "%*.*s", ( gint ) bytes_read, ( gint ) bytes_read, buffer );
+				}
+				g_print( "\n" );
+				break;
+			}
+			archive_read_data_skip( archive );
+		}
+		g_free( searched );
+		g_free( buffer );
+	}
+
+	g_free( pathname );
+	g_object_unref( file );
+	archive_read_close( archive );
+	archive_read_free( archive );
+
+	return( found ? 0 : 1 );
+}
+
+static gint
+dump_data( const gchar *basename, const gchar *uri, const gchar *name )
+{
+	static const gchar *thisfn = "ofa_unzip_dump_data";
+	GFile *file;
+	gchar *pathname, *buffer, *searched;
+	struct archive *archive;
+	struct archive_entry *entry;
+	const gchar *cname;
+	gboolean found;
+	gsize bytes_read;
+
+	found = FALSE;
+	archive = archive_read_new();
+	archive_read_support_filter_all( archive );
+	archive_read_support_format_all( archive );
+
+	file = g_file_new_for_uri( uri );
+	pathname = g_file_get_path( file );
+	if( archive_read_open_filename( archive, pathname, 16384 ) != ARCHIVE_OK ){
+		g_warning( "%s: archive_read_open_filename: path=%s, %s", thisfn, pathname, archive_error_string( archive ));
+
+	} else {
+		buffer = g_new0( gchar, BUFSIZE );
+		searched = g_strdup_printf( "%s%s", OFA_BACKUP_HEADER_HEADER, name );
+		while( archive_read_next_header( archive, &entry ) == ARCHIVE_OK ){
+			cname = archive_entry_pathname( entry );
+			if( !g_strcmp0( cname, searched )){
+				found = TRUE;
+				g_print( _( "[%s] dumping stream '%s':\n" ), basename, cname );
+				while(( bytes_read = archive_read_data( archive, buffer, BUFSIZE )) > 0 ){
+					g_print( "%*.*s", ( gint ) bytes_read, ( gint ) bytes_read, buffer );
+				}
+				g_print( "\n" );
+				break;
+			}
+			archive_read_data_skip( archive );
+		}
+		g_free( searched );
+		g_free( buffer );
+	}
+
+	g_free( pathname );
+	g_object_unref( file );
+	archive_read_close( archive );
+	archive_read_free( archive );
+
+	return( found ? 0 : 1 );
+}
+
+#if 0
 static gint
 extract_archive( const gchar *uri )
 {
@@ -93,30 +250,56 @@ extract_archive( const gchar *uri )
 
 	return( found ? 0 : 1 );
 }
+#endif
 
 int
 main( int argc, char *argv[] )
 {
-	gchar *basename;
+	gchar *basename, *msg;
 	gint status;
+	GOptionContext *context;
+	GError *error;
 
 	if( argc < 1 ){
-		g_warning( "Expected to get program name from first 'argv' argument, which is empty.\nAborting." );
+		g_warning( _( "Expected to get program name from first 'argv' argument, which is empty.\nAborting." ));
 		exit( 1 );
 	}
 
 	basename = g_path_get_basename( argv[0 ] );
 	status = 0;
+	error = NULL;
 
-	if( argc != 2 ){
-		g_print( "\n"
-				" %s: extract data member of an archive file to stdout.\n"
-				" Usage:\n"
-				"   %s <archive_uri>\n"
-				"\n",
-				basename, basename );
-	} else {
-		status = extract_archive( argv[1] );
+	context = g_option_context_new( _( "- dump a zip file" ));
+	g_option_context_add_main_entries( context, st_entries, GETTEXT_PACKAGE );
+	g_option_context_add_group( context, gtk_get_option_group( TRUE ));
+
+	if( argc < 2 ){
+		msg = g_option_context_get_help( context, TRUE, NULL );
+		g_print( _( "%s: %s\n" ), basename, msg );
+		g_free( msg );
+		exit( 0 );
+	}
+
+	if( !g_option_context_parse( context, &argc, &argv, &error )){
+		g_print( _( "%s: option parsing failed: %s\n" ), basename, error->message );
+		exit( 1 );
+	}
+
+	if( !my_strlen( st_uri )){
+		g_warning( _( "%s: the URI of the file to be dumped is mandatory\n" ), basename );
+		exit( 1 );
+	}
+
+	g_debug( "URI='%s'", st_uri );
+
+	if( my_strlen( st_header )){
+		status += dump_header( basename, st_uri, st_header );
+
+	} else if( my_strlen( st_data )){
+		status += dump_data( basename, st_uri, st_data );
+
+	} else if( st_list ){
+		status += list_archive( basename, st_uri );
 	}
 
 	g_free( basename );
